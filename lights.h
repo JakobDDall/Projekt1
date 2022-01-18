d20b30875f3b61a8f8c830531a0d90320cc855be
FrederikKongstedMadsen <frederik.51@live.dk>
2022-01-18 12:12:01 +0100

    Final pis
    Jaja send lortet

--- lights.h
+++
@@ -1,8 +0,0 @@
-/*
- * Created: 19/11/2021 12.27.42
- * Author : Genku(M.T.A.)
- */ 
-
-void initLight();
-void frontLightState(int state); //state = 0/1 = off/on
-void backLightState(int state); //state = 0/1/2 = off/on/braking
--- reflex.c
+++ reflex.c
@@ -4,8 +4,7 @@ void initReflex(void)
 {
 	// INT0: rising edge
 	EICRA = 0b00000000;
-	EICRB = 0b00001100;	//Something wrong here. Reflex is not detected!!!
-	// Enable extern interrupts INT0
+	EICRB = 0b00001100;	// Enable extern interrupts INT5
 	EIMSK |= 0b00100000;
 	
 }
